e25e13223944203136fce9bcbc849fde01e38ab4
dimoonoomid <dimoonoomid@yandex.ru>
2024-02-04 11:39:56 +0300

    9 sprint homework. Setting Back button changed

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -3,14 +3,14 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val setting_backButton = findViewById<Button>(R.id.setting_backButton)
+        val setting_backButton = findViewById<ImageView>(R.id.SettingBackArrowImage)
         setting_backButton.setOnClickListener{
             val intent = Intent(this@SettingActivity,MainActivity::class.java)
             startActivity(intent)
