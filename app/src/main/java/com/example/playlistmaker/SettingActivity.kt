d5c04c63baf0dae1d5d450711669d3d6ec9c2d47
dimoonoomid <dimoonoomid@yandex.ru>
2024-02-10 23:09:04 +0300

    9 sprint homework. Night theme without TapBar. SettingActivity ArrowBack Deleted

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -1,19 +1,17 @@
 package com.example.playlistmaker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val setting_backButton = findViewById<ImageView>(R.id.SettingBackArrowImage)
-        setting_backButton.setOnClickListener{
-            val intent = Intent(this@SettingActivity,MainActivity::class.java)
-            startActivity(intent)
-        }
+
+
     }
 }
