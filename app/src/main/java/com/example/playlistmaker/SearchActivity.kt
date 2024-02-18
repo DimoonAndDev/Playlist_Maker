fa66012028cf05d1a31a24eb9e17a7a09c1867a1
dimoonoomid <dimoonoomid@yandex.ru>
2024-02-18 16:32:34 +0300

    9 sprint homework. TabBar deleted, Back arrows added. SearchActivity EditText made, SettingActivity Dark theme switcher activated

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,59 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.content.res.AppCompatResources
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchBackArrowImage = findViewById<ImageView>(R.id.SearchBackArrowImage)
+        val searchEditText = findViewById<EditText>(R.id.SearchEditText)
+        val searchClearTextImage = findViewById<ImageView>(R.id.SearchClearTextImage)
+
+
+        searchBackArrowImage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        searchEditText.requestFocus()
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.showSoftInput(searchEditText, 0)
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { //nothing
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (p0.isNullOrEmpty()) {
+                    searchClearTextImage.visibility = View.INVISIBLE
+                } else searchClearTextImage.visibility = View.VISIBLE
+            }
+
+            override fun afterTextChanged(p0: Editable?) {//nothing
+            }
+        }
+        searchEditText.addTextChangedListener(searchTextWatcher)
+
+        searchClearTextImage.setImageDrawable(AppCompatResources.getDrawable(this,R.drawable.clearsearchbutton))
+        searchClearTextImage.setOnClickListener {
+            searchEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
     }
+
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -1,22 +1,43 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val ShareButton = findViewById<Button>(R.id.ShareButton)
-        val SupportButton = findViewById<Button>(R.id.SupportButton)
-        val UserCondButton = findViewById<Button>(R.id.UserCondButton)
+        val backArrowImage = findViewById<ImageView>(R.id.SettingsBackArrowImage)
+        val switchDarkMode = findViewById<SwitchCompat>(R.id.SettingSwitchDarkTheme)
+        val shareButton = findViewById<Button>(R.id.ShareButton)
+        val supportButton = findViewById<Button>(R.id.SupportButton)
+        val userCondButton = findViewById<Button>(R.id.UserCondButton)
+
+        backArrowImage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
-        ShareButton.setOnClickListener {
+
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK){
+            Configuration.UI_MODE_NIGHT_YES-> switchDarkMode.isChecked = true
+            Configuration.UI_MODE_NIGHT_NO-> switchDarkMode.isChecked = false
+        }
+        switchDarkMode.setOnClickListener {
+            if (switchDarkMode.isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        shareButton.setOnClickListener {
             val shareIntent = Intent()
             val message = getString(R.string.YP_link_array)
             shareIntent.action = Intent.ACTION_SEND
@@ -25,7 +46,7 @@ class SettingActivity : AppCompatActivity() {
             startActivity(shareIntent)
         }
 
-        SupportButton.setOnClickListener {
+        supportButton.setOnClickListener {
             val message = getString(R.string.YP_support_message)
             val supportIntent = Intent(Intent.ACTION_SENDTO)
             supportIntent.data = Uri.parse("mailto:")
@@ -34,7 +55,7 @@ class SettingActivity : AppCompatActivity() {
             supportIntent.putExtra(Intent.EXTRA_TEXT, message)
             startActivity(supportIntent)
         }
-        UserCondButton.setOnClickListener {
+        userCondButton.setOnClickListener {
             val url = getString(R.string.YP_user_cond_link)
             val userCondIntent = Intent(Intent.ACTION_VIEW)
             userCondIntent.data = Uri.parse(url)
